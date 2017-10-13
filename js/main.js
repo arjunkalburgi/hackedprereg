7f4248af8eaea8b1d5e9d7d1e057c2f0f972a082
Arjun Kalburgi <askalburgi@gmail.com>
2017-10-13 11:20:44 -0600

    lessening the glow on the symbols

--- js/main.js
+++ js/main.js
@@ -23,25 +23,33 @@ function chooseBackgroundColour() {
             //RED
             $("body").css("color", "#E83223"); 
             $(".button").css({"color" : "#E83223", "border" : "1pt solid #E83223" }); 
-            $("body .hdr-text").addClass("redneon"); 
+            $("body .hacked").addClass("redneon-text"); 
+            $("body .prompt").addClass("redneon-symbols"); 
+            $("body .cursor").addClass("redneon-symbols"); 
             break; 
         case 2: 
             //YELLOW
             $("body").css("color", "#FCED51");
             $(".button").css({"color" : "#FCED51", "border" : "1pt solid #FCED51" });
-            $("body .hdr-text").addClass("yellowneon"); 
+            $("body .hacked").addClass("yellowneon-text"); 
+            $("body .prompt").addClass("yellowneon-symbols"); 
+            $("body .cursor").addClass("yellowneon-symbols"); 
             break; 
         case 3: 
             //GREEN
             $("body").css("color", "#76FA46");
             $(".button").css({"color" : "#76FA46", "border" : "1pt solid #76FA46" });
-            $("body .hdr-text").addClass("greenneon"); 
+            $("body .hacked").addClass("greenneon-text"); 
+            $("body .prompt").addClass("greenneon-symbols"); 
+            $("body .cursor").addClass("greenneon-symbols"); 
             break; 
         case 4: 
             //BLUE
             $("body").css("color", "#5d8cf7"); 
             $(".button").css({"color" : "#5d8cf7", "border" : "1pt solid #5d8cf7" }); 
-            $("body .hdr-text").addClass("blueneon"); 
+            $("body .hacked").addClass("blueneon-text"); 
+            $("body .prompt").addClass("blueneon-symbols"); 
+            $("body .cursor").addClass("blueneon-symbols"); 
             break; 
     } 
 }
